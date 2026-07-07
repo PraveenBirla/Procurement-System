61be4789875b55e8bb990bff8f5189d20a04d1ff
Sathya Sri P V <sathyasripv@gmail.com>
2026-07-07 18:03:48 +0530

    intializing spring boot

---
+++ enterprise-procurement-system/src/main/java/com/eps/enterprise_procurement_system/EnterpriseProcurementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.eps.enterprise_procurement_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseProcurementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseProcurementSystemApplication.class, args);
+	}
+
+}
