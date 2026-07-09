d2936218b687a741d8637b68b7969a02974cc00b
Vishnu <vishnug1705@gmail.com>
2026-07-09 11:29:25 +0530

    Added Department Entity

---
+++ backend/src/main/java/com/eps/enterprise_procurement_system/entities/Department.java
@@ -0,0 +1,25 @@
+package com.eps.enterprise_procurement_system.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "departments")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "department_name", nullable = false)
+    private String departmentName;
+
+}
