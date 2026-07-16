8886cedb049226819b2644cb81bcb042902b2466
panja abhishek <panjaabhishek57@gmail.com>
2026-07-16 13:34:21 +0530

    feat(db): add initial seed data for testing

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO department (name) VALUES ('Engineering');
+INSERT INTO department (name) VALUES ('Finance');
+INSERT INTO department (name) VALUES ('Operations');
+INSERT INTO department (name) VALUES ('Procurement');
+INSERT INTO department (name) VALUES ('Human Resources');
+INSERT INTO department (name) VALUES ('Sales');
